eafd63f1f3b9e7aed1fc1b07e19ef02ff86d40c0
luochongfei <luochongfei@126.com>
2018-01-22 22:56:48 +0800

    基本信息适配

---
+++ props.js
@@ -0,0 +1,104 @@
+var props = {
+    "attr_list": [
+        {
+            "name": "Size",
+            "attr_id": "7",
+            "list": [
+                {
+                    "prime": 2,
+                    "attr_value": "26",
+                    "is_check": ""
+                },
+                {
+                    "prime": 3,
+                    "attr_value": "28",
+                    "is_check": ""
+                }
+            ],
+            "idx": 0
+        },
+        {
+            "name": "Color",
+            "attr_id": "8",
+            "list": [
+                {
+                    "prime": 5,
+                    "attr_value": "WHITE",
+                    "is_check": ""
+                },
+                {
+                    "prime": 7,
+                    "attr_value": "PINK",
+                    "is_check": ""
+                },
+                {
+                    "prime": 11,
+                    "attr_value": "PURPLE",
+                    "is_check": ""
+                }
+            ],
+            "idx": 1
+        }
+    ],
+    "goods_list": [
+        {
+            "product_number": 10,
+            "goods_id": "1034713",
+            "goods_title": "Stylish Warmest Homelike LED Half Unicorn Slippers",
+            "goods_img": "https://gloimg.gbtcdn.com/gb/pdm-product-pic/Electronic/2017/10/24/goods-img/1510311234830648499.jpg",
+            "stock": 1,
+            "price": "20.53"
+        },
+        {
+            "product_number": 15,
+            "goods_id": "1034726",
+            "goods_title": "Stylish Warmest Homelike LED Half Unicorn Slippers",
+            "goods_img": "https://gloimg.gbtcdn.com/gb/pdm-product-pic/Electronic/2017/10/24/goods-img/1508818683095227663.jpg",
+            "stock": 1,
+            "price": "20.53"
+        },
+        {
+            "product_number": 21,
+            "goods_id": "1034739",
+            "goods_title": "Stylish Warmest Homelike LED Half Unicorn Slippers",
+            "goods_img": "https://gloimg.gbtcdn.com/gb/pdm-product-pic/Electronic/2017/10/24/goods-img/1510311238238011325.jpg",
+            "stock": 1,
+            "price": "20.53"
+        },
+        {
+            "product_number": 14,
+            "goods_id": "1034755",
+            "goods_title": "Stylish Warmest Homelike LED Half Unicorn Slippers",
+            "goods_img": "https://gloimg.gbtcdn.com/gb/pdm-product-pic/Electronic/2017/10/24/goods-img/1508818689821435626.jpg",
+            "stock": 1,
+            "price": "20.53"
+        },
+        {
+            "product_number": 33,
+            "goods_id": "1034766",
+            "goods_title": "Stylish Warmest Homelike LED Half Unicorn Slippers",
+            "goods_img": "https://gloimg.gbtcdn.com/gb/pdm-product-pic/Electronic/2017/10/24/goods-img/1510311241818635511.jpg",
+            "stock": 1,
+            "price": "20.53"
+        },
+        {
+            "product_number": 22,
+            "goods_id": "1034778",
+            "goods_title": "Stylish Warmest Homelike LED Half Unicorn Slippers",
+            "goods_img": "https://gloimg.gbtcdn.com/gb/pdm-product-pic/Electronic/2017/10/24/goods-img/1508818694383984637.jpg",
+            "stock": 1,
+            "price": "20.53"
+        }
+    ],
+    "prime_list": {
+        "7": {
+            "26": 2,
+            "28": 3
+        },
+        "8": {
+            "WHITE": 5,
+            "PINK": 7,
+            "PURPLE": 11
+        }
+    }
+}
